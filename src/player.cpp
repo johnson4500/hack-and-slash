bdca346dab9cdb0de74a26bf333fc2ca45030aaf
johnson4500 <johnsonhan6500@gmail.com>
2024-04-13 17:56:54 -0400

    Load textures with TextureManager class

--- src/player.cpp
+++ src/player.cpp
@@ -1,33 +1,34 @@
 #include <player.hpp>
 #include <iostream>
 #include <animation.hpp>
+#include <textureManager.hpp>
 
-Player::Player(sf::Texture &playerTexture, sf::IntRect &sourceSprite, sf::Vector2f playerPosition) {
-    this->sourceSprite = sourceSprite;
-    this->playerTexture = playerTexture;
-    this->player = sf::Sprite(playerTexture, sourceSprite);
+Player::Player(sf::Vector2f playerPosition) {
+    // loadTexture() returns a pointer
+    TextureManager::loadTexture("playeridle", "assets/playeridle.png");
+    TextureManager::loadTexture("playerrun", "assets/playerrun.png");
+    this->playerTexture = TextureManager::textureMap["playeridle"];
     this->sourceSprite.left = 0;
-    this->player.setPosition(playerPosition);
-    this->player.setOrigin(160, 160);
-    this->player.setScale(1.0f, 1.0f);
+    this->playerSprite.setPosition(playerPosition);
+    this->playerSprite.setOrigin(160, 160);
+    this->playerSprite.setScale(1.0f, 1.0f);
     this->playerSpeed = 100;
-    animator = new Animation(&playerTexture, &sourceSprite, &player);
+    animator = new Animation(playerTexture, &sourceSprite, &playerSprite);
 };
 
 void Player::playerMove(float dt) {
-    playerDirection = 1;
-    player.move(playerSpeed * dt, 0);
+    playerSprite.move(playerSpeed * dt * playerDirection, 0);
     playerIsRight = false;
-    player.setScale(1.0f, 1.0f);
+    playerSprite.setScale(1.0f * playerDirection, 1.0f);
     // if (!isAttacking && !dodging) {
     //     animate(elapsed, c2, sourceSprite, player, playerTextureLeft);
     // }
     // animator->play();
 };
 
-void Player::changeAnimation(sf::Texture &newTexture, sf::IntRect &newTextureRect) {
-    this->player.setTexture(newTexture);
-    animator->setTexture(&newTexture, &newTextureRect);
+void Player::changeAnimation(sf::Texture *newTexture, sf::IntRect &newTextureRect) {
+    this->playerSprite.setTexture(*newTexture);
+    animator->setTexture(newTexture, &newTextureRect);
 }
 
 void Player::animate() {
