41bef4bc923c29f5f09fc4a3c480d0d8da1ce862
johnson4500 <johnsonhan6500@gmail.com>
2024-04-13 20:35:14 -0400

    Implement transitions between rooms

--- src/levelone.cpp
+++ src/levelone.cpp
@@ -10,10 +10,49 @@ LevelOne::LevelOne() {
         skyTextures[i] = TextureManager::loadTexture(name, path);
         skySprites[i] = sf::Sprite();
         skySprites[i].setTexture(*skyTextures[i]);
-        skySprites[i].setScale(2.25, 2.25);
-        skySprites[i].setPosition(0, -750);
+        skySprites[i].setScale(2.5, 2.5);
+        skySprites[i].setPosition(0, -875);
     }
+
+    font.loadFromFile("C:/Users/johns/Downloads/arial/arial.ttf");
+    doorText.setFont(font);
+    doorText.setCharacterSize(25); // in pixels, not points
+    doorText.setFillColor(sf::Color::White);
+    doorText.setString(std::string("Press E to enter"));
+
+    transitionRect.setFillColor(sf::Color::Black);
+    transitionRect.setSize(sf::Vector2f(1600, 900));
+    transitionRect.setFillColor(sf::Color(0, 0, 0, alphavalue));
 };
+
+void LevelOne::update(Player *player, sf::RenderWindow &window) {
+    if (player->playerSprite.getPosition().x >= 1162 && player->playerSprite.getPosition().x <= 1240) {
+        player->doorInteract = true;
+        doorText.setPosition(player->playerSprite.getPosition().x + 30, player->playerSprite.getPosition().y - 50); 
+        window.draw(doorText);
+
+        if (playTransition) {
+            transition(window);
+        }
+    } else {
+        player->doorInteract = false;
+    }
+}
+
+void LevelOne::transition(sf::RenderWindow &window) {
+    transitionRect.setPosition(600, -100);
+    fadeClockElapsed = fadeClock.getElapsedTime();
+    if (fadeClockElapsed.asSeconds() >= 0.005) {
+        if (alphavalue < 255) {
+            transitionRect.setFillColor(sf::Color(0, 0, 0, alphavalue));
+            alphavalue+= 5;
+        }
+        fadeClock.restart();
+    }
+
+    window.draw(transitionRect);
+}
+
 
 void LevelOne::render(sf::RenderWindow &window) {
     for (int i = 0; i < 7; i++) {
--- src/levelone.hpp
+++ src/levelone.hpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <player.hpp>
 #include <iostream>
 #include <string>
 #include <format>
@@ -8,8 +9,20 @@ class LevelOne {
         sf::Texture* skyTextures[7];
         sf::Sprite skySprites[7];
         float skyParallaxValues[7] = {1, 1.5, 3.8, 7.5, 25, 50, 80};
+        sf::Font font;
+        sf::Text doorText;
+    
+        bool playTransition = false;
+        sf::Clock fadeClock;
+        sf::Time fadeClockElapsed;
+        sf::RectangleShape transitionRect;
+        int alphavalue = 0;
 
         LevelOne();
+
+        void update(Player *player, sf::RenderWindow &window);
+
+        void transition(sf::RenderWindow &window);
 
         void render(sf::RenderWindow &window);
 
